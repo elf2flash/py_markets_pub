d20a0f7a9039f5dff8a71f0c840ef9042fc90a70
elf2flash <elf2flash@gmail.com>
2020-04-10 17:11:04 +0500

    Collection of data from file to array is finish

--- check_history_data/check_history_data.py
+++ check_history_data/check_history_data.py
@@ -1,69 +1,98 @@
+import bot0
+#import matplotlib.pyplot as plt
+
 #*************************************************************************#
-def f_find_str_inx0_inx1( tmp_str, inx1 ):
+def f_find_str_inx0_inx1( TmpStr, inx1 ):
     inx0 = inx1 + 1
-    inx1 = tmp_str.find(';',inx0)
-    s = tmp_str[inx0:inx1]
-    return [ s , inx0 , inx1 ]
+    inx1 = TmpStr.find(';',inx0)
+    S = TmpStr[inx0:inx1]
+    return [ S , inx0 , inx1 ]
+
+
+
 
 #*************************************************************************#
 #############################       MAIN      #############################
 #*************************************************************************#
-#str_file_name = 'GAZP_200406_200406.txt'
-str_file_name = 'GAZP_200406_200406_small.txt'
+StrFileName = 'GAZP_200406_200406_small.txt'
+StrFileName = 'GAZP_200406_200406.txt'
+StrFileName = 'GAZP_200101_200408.txt'
+StrFileName = 'test.txt'
+print(StrFileName)
 
+pFile = open(StrFileName)
+FileLine = pFile.readline()
 
-f = open(str_file_name)
-line = f.readline()
+ArrKData = []
 k = 0
-while line:
+while FileLine:
     if (k>0):
         
         ArrFileString = []
         
         inx0 = 0
-        inx1 = line.find(';',0)
-        tmp_str = line[inx0:inx1]
-        ArrFileString.append(tmp_str)
+        inx1 = FileLine.find(';',0)
+        # if end file, then exit
+        if (inx1 < 0):
+            break
+        TmpStr = FileLine[inx0:inx1]
+        ArrFileString.append(TmpStr)
         for j in range(8):
-            [tmp_str, inx0, inx1] = f_find_str_inx0_inx1( line, inx1)
-            ArrFileString.append(tmp_str)
-        for i in ArrFileString:
-            print(i)
-        print('----------')
-
+            [TmpStr, inx0, inx1] = f_find_str_inx0_inx1( FileLine, inx1)
+            ArrFileString.append(TmpStr)
+        # collect and trasnform data
         ArrFileValue = []
-        ArrFileValue.append(ArrFileString[0])
-        ArrFileValue.append(ArrFileString[1])
-        ArrFileValue.append(int(ArrFileString[2][0:4]))
-        ArrFileValue.append(int(ArrFileString[2][4:6]))
-        ArrFileValue.append(int(ArrFileString[2][5:7]))
-        ArrFileValue.append(int(ArrFileString[3][0:2]))
-        ArrFileValue.append(int(ArrFileString[3][2:4]))
-        ArrFileValue.append(int(ArrFileString[3][4:6]))
-        ArrFileValue.append(float(ArrFileString[4]))
-        ArrFileValue.append(float(ArrFileString[5]))
-        ArrFileValue.append(float(ArrFileString[6]))
-        ArrFileValue.append(float(ArrFileString[7]))
-        ArrFileValue.append(int(ArrFileString[8]))
-
-        j = 0
-        for i in ArrFileValue:
-            print('j=' + str(j) + " : " + str(i) )
-            j = j + 1
-
-        print(ArrFileValue[8])
-        print(float(ArrFileValue[8])+1)
-
-
+        ArrFileValue.append(ArrFileString[0])               # 0
+        ArrFileValue.append(ArrFileString[1])               # 1
+        ArrFileValue.append(int(ArrFileString[2][0:4]))     # 2 year
+        ArrFileValue.append(int(ArrFileString[2][4:6]))     # 3 month
+        ArrFileValue.append(int(ArrFileString[2][5:7]))     # 4 day
+        ArrFileValue.append(ArrFileString[3][0:2])          # 5 hour
+        ArrFileValue.append(ArrFileString[3][2:4])          # 6 minute
+        ArrFileValue.append(ArrFileString[3][4:6])          # 7 secund
+        ArrFileValue.append(float(ArrFileString[4]))        # 8  OPEN
+        ArrFileValue.append(float(ArrFileString[5]))        # 9  HIGH
+        ArrFileValue.append(float(ArrFileString[6]))        # 10 LOW
+        ArrFileValue.append(float(ArrFileString[7]))        # 11 CLOSE
+        ArrFileValue.append(int(ArrFileString[8]))          # 12 VOL
+        #collect data to 2D array
+        ArrKData.append(ArrFileValue)
         
-        #print(CN_date + " " + str(inx0) + " " + str(inx1))
-        #[CN_time, inx0, inx1] = f_find_str_inx0_inx1( tmp_str, inx1)
-        
-        break
-    #print (line),
-    line = f.readline()
+        if (1==0):
+            j = 0
+            for i in ArrFileValue:
+                print('j=' + str(j) + " : " + str(i) )
+                j = j + 1
+    FileLine = pFile.readline()
     k = k + 1
-    
-f.close()
+pFile.close()
+
+
+#-----------------------------------------------------------------------------------------------
+# Work with ArrKData
+LData = len(ArrKData)
+print("LData = " + str(LData))
+
+bot0.f_bot0( ArrKData, LData )
+
+
+
+#fig, ax = plt.subplots()                        # будет 1 график, на нем:
+#x = range(0,LData)
+#H = []
+#L = []
+#for i in range(0,LData):
+#    CurrData = ArrKData[i]
+#    H.append(CurrData[9])
+#    L.append(CurrData[10])
+#ax.plot(x, H, color="blue", label="y(x)",linestyle="-")      # функция y1(x), синий, надпись y(x)
+#ax.plot(x, L, color="red", label="y'(x)",linestyle="-")      # функция y2(x), красный, надпись y'(x)
+#ax.set_xlabel("x")                              # подпись у горизонтальной оси х
+#ax.set_ylabel("y")                              # подпись у вертикальной оси y
+#ax.legend()                                     # показывать условные обозначения
+#plt.show()                                      # показать рисунок
+
+print("end")
+
 
 
